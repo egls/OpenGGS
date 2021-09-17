0bac4e458a54208eaac17d1b33f2452a13b84558
stefan <egli.stefan@gmail.com>
2021-09-17 12:35:12 -0700

    removed obsolete defines

--- src/Audio.hpp
+++ src/Audio.hpp
@@ -1,19 +1,3 @@
-// #define AUDIO_DISK 0
-// #define AUDIO_JUMP 1
-// #define AUDIO_DING 2
-// #define AUDIO_CRACK 3
-// #define AUDIO_STOMP 4
-// #define AUDIO_KILLED 5
-// #define AUDIO_GOTPOWERUP 6
-// #define AUDIO_PORTAL 7
-// #define AUDIO_CLICK 8
-// #define AUDIO_SHOOT 9
-
-// #define MUSIC_MENU 0
-// #define MUSIC_OUTDOORS 1
-// #define MUSIC_INDOORS 2
-// #define MUSIC_HIGHSCORE 3
-
 class Audio
 {
 
